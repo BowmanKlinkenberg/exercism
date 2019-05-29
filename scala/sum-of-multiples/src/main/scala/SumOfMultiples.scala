5f369dc8a97eb5d7a8e02a8bb678eea56d1dd1a4
Bowman Klinkenberg <bowman.klinkenberg@target.com>
2019-05-29 16:08:37 -0500

    added sum of multiples problem

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,4 +1,18 @@
 object SumOfMultiples {
-  def sum(factors: Set[Int], limit: Int): Int = ???
+  def sum(factors: Set[Int], limit: Int): Int = {
+    val multiples: Array[Int] = for ( i <- 1 until limit if  ){
+      for (f <- factors ){
+        if()
+      }
+    }
+  }
+
+  def isAnyDiv(factors: Set[Int], i: Int): Boolean = {
+    // return true if i is divisible by any of the factors
+    for (f <- factors){
+      if(f % i == 0) true else false
+    }
+  }
+
 }
 
