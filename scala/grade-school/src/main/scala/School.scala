095c263b72cc697b75906aad1514e70d782f443f
Bowman Klinkenberg <bowman.klinkenberg@target.com>
2019-06-26 15:07:19 -0500

    working gradeschool oo tests

--- scala/grade-school/src/main/scala/School.scala
+++ scala/grade-school/src/main/scala/School.scala
@@ -1,11 +1,21 @@
 class School {
-  type DB = Map[Int, Seq[String]]
+  type DB = collection.mutable.Map[Int, Seq[String]]
 
-  def add(name: String, g: Int) = ???
+  private val _db: DB = collection.mutable.Map()
 
-  def db: DB = ???
+  def db: DB = _db
 
-  def grade(g: Int): Seq[String] = ???
+  def add(name: String, g: Int): Unit = _db.get(g) match {
+    case None => _db(g) = Seq(name)
+    case _ => _db(g) = grade(g) :+ name
+  }
 
-  def sorted: DB = ???
+  def grade(g: Int): Seq[String] = _db.get(g) match {
+    case None => Seq()
+    case _ => _db(g)
+  }
+
+  def sorted: Map[Int, Seq[String]] = {
+    collection.immutable.TreeMap(_db.toSeq:_*).mapValues(_.sorted)
+  }
 }
